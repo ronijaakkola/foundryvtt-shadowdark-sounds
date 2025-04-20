54ea071f748e46d6cf2d8292178ccdf862146f9c
Roni Jaakkola <roni.jaakkola@gmail.com>
2025-04-20 14:22:01 +0300

    Refactor audio playback functions

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,4 +1,17 @@
-export function playAudioOneShot(soundPath, volume, push = true) {
+export function playAudio(soundName) {
+    if (!game.settings.get("shadowdark-sounds", "shadowdark-sounds-enabled")) {
+        return result;
+    }
+
+    if (!soundName) return;
+
+    const soundPath = game.settings.get("shadowdark-sounds", `${soundName}-sound`);
+    const soundVolume = game.settings.get("shadowdark-sounds", `${soundName}-volume`);
+
+    playAudioOneShot(soundPath, soundVolume, true);
+}
+
+export function playAudioOneShot(soundPath, volume, pushToPlayers = true) {
     if (!soundPath) return;
 
     foundry.audio.AudioHelper.play({
@@ -6,5 +19,5 @@ export function playAudioOneShot(soundPath, volume, push = true) {
         volume: volume ?? 0.5,
         loop: false,
         autoplay: true,
-    }, push)
+    }, pushToPlayers)
 }
--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,4 +1,4 @@
-import { playAudioOneShot } from "./helpers.js";
+import { playAudio, playAudioOneShot } from "./helpers.js";
 
 export function registerHooks() {
 
@@ -9,25 +9,15 @@ export function registerHooks() {
         "game.system.apps.LightSourceTrackerSD.prototype.toggleLightSource",
         async function (wrapped, ...args) {
             const result = await wrapped.apply(this, args);
-            
-            if (!game.settings.get("shadowdark-sounds", "shadowdark-sounds-enabled")) {
-                return result;
-            }
 
             if (args[1]?.system.light.active) {
                 if (game.settings.get("shadowdark-sounds", "torch-ignite-enabled")) {
-                    playAudioOneShot(
-                        game.settings.get("shadowdark-sounds", "torch-ignite-sound"),
-                        game.settings.get("shadowdark-sounds", "torch-ignite-volume")
-                    );
+                    playAudio("torch-ignite");
                 }
             }
             else {
                 if (game.settings.get("shadowdark-sounds", "torch-douse-enabled")) {
-                    playAudioOneShot(
-                        game.settings.get("shadowdark-sounds", "torch-douse-sound"),
-                        0.5
-                    );
+                    playAudio("torch-douse");
                 }
             }
 
@@ -43,16 +33,9 @@ export function registerHooks() {
         "game.system.apps.LightSourceTrackerSD.prototype.dropLightSourceOnScene",
         async function (wrapped, ...args) {
             const result = await wrapped.apply(this, args);
-            
-            if (!game.settings.get("shadowdark-sounds", "shadowdark-sounds-enabled")) {
-                return result;
-            }
 
             if (game.settings.get("shadowdark-sounds", "torch-drop-enabled")) {
-                playAudioOneShot(
-                    game.settings.get("shadowdark-sounds", "torch-drop-sound"),
-                    game.settings.get("shadowdark-sounds", "torch-drop-volume")
-                );
+                playAudio("torch-drop");
             }
 
             return result;
@@ -67,16 +50,9 @@ export function registerHooks() {
         "game.system.apps.LightSourceTrackerSD.prototype.pickupLightSourceFromScene",
         async function (wrapped, ...args) {
             const result = await wrapped.apply(this, args);
-            
-            if (!game.settings.get("shadowdark-sounds", "shadowdark-sounds-enabled")) {
-                return result;
-            }
 
             if (game.settings.get("shadowdark-sounds", "torch-pickup-enabled")) {
-                playAudioOneShot(
-                    game.settings.get("shadowdark-sounds", "torch-pickup-sound"),
-                    game.settings.get("shadowdark-sounds", "torch-pickup-volume")
-                );
+                playAudio("torch-pickup");
             }
 
             return result;
@@ -91,16 +67,9 @@ export function registerHooks() {
         "game.system.documents.ActorSD.prototype.yourLightExpired",
         async function (wrapped, ...args) {
             const result = await wrapped.apply(this, args);
-            
-            if (!game.settings.get("shadowdark-sounds", "shadowdark-sounds-enabled")) {
-                return result;
-            }
 
             if (game.settings.get("shadowdark-sounds", "torch-expire-enabled")) {
-                playAudioOneShot(
-                    game.settings.get("shadowdark-sounds", "torch-expire-sound"),
-                    game.settings.get("shadowdark-sounds", "torch-expire-volume")
-                );
+                playAudio("torch-expire");
             }
 
             return result;
